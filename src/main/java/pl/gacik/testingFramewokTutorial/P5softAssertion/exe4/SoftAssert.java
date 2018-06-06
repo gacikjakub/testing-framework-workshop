4322a260b92ebb624a0294d5c685ef364fdf9630
JG <gacik.jakub@gmail.com>
2018-06-06 16:20:29 +0200

    Exe4 prepared

--- src/main/java/pl/gacik/testingFramewokTutorial/P5softAssertion/exe4/SoftAssert.java
+++ src/main/java/pl/gacik/testingFramewokTutorial/P5softAssertion/exe4/SoftAssert.java
@@ -3,13 +3,7 @@ package pl.gacik.testingFramewokTutorial.P5softAssertion.exe4;
 public class SoftAssert {
 
     public static void assertion(boolean condition, String failMessage) {
-        try {
-            assert condition : failMessage;
-        } catch (Throwable t) {
-            System.err.println(t);
-            t.printStackTrace();
-            System.err.println();
-        }
+
     }
 
 }
--- src/main/java/pl/gacik/testingFramewokTutorial/P6extras/SP1softAssertionExtended/exe5/SoftAssert.java
+++
@@ -1,7 +0,0 @@
-package pl.gacik.testingFramewokTutorial.P6extras.SP1softAssertionExtended.exe5;
-
-public class SoftAssert {
-
-    // Implement the missing method
-
-}
