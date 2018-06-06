f9fd59cde5e2c019c77d06f35627f50177da07d4
JG <gacik.jakub@gmail.com>
2018-06-06 10:24:54 +0200

    Exe3 solved

--- src/main/java/pl/gacik/testingFramewokTutorial/P4ensuringIndependenceOfTests/exe3/TestClass.java
+++ src/main/java/pl/gacik/testingFramewokTutorial/P4ensuringIndependenceOfTests/exe3/TestClass.java
@@ -4,7 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
@@ -18,16 +17,15 @@ public class TestClass {
 
     public static void runClassTests(Object o) {
         Method[] declaredMethods = o.getClass().getDeclaredMethods();
-        Arrays.stream(declaredMethods).filter(m -> m.isAnnotationPresent(pl.gacik.testingFramewokTutorial.P4ensuringIndependenceOfTests.standard.TestClass.Test.class)).forEach(m -> {
+        Arrays.stream(declaredMethods).filter(m -> m.isAnnotationPresent(Test.class)).forEach(m -> {
             System.out.println(m.getName());
-
-            // Refactor this code so that all tests are performed regardless of the result.
             try {
                 m.invoke(o);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
+            } catch (Throwable t) {
+                t.printStackTrace();
             }
         });
+
     }
 
     public static void main(String[] args) {
