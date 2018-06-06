1d408ef45176ca16da382471102fb1e49160a83e
JG <gacik.jakub@gmail.com>
2018-06-06 09:45:08 +0200

    Exe1 prepared

---
+++ src/main/java/pl/gacik/testingFramewokTutorial/P1SimpleAssert/exe1/SimpleAssert.java
@@ -0,0 +1,13 @@
+package pl.gacik.testingFramewokTutorial.P1SimpleAssert.exe1;
+
+public class SimpleAssert {
+
+    public static void assertion(boolean condition, String failMessage) {
+        // Write you own implementation of keyword assert. As simple as you can.
+    }
+
+    public static void main(String[] args) {
+        assertion(false, "Sth wrong");
+    }
+
+}
---
+++ src/main/java/pl/gacik/testingFramewokTutorial/P1SimpleAssert/exe1/StandardAssert.java
@@ -0,0 +1,7 @@
+package pl.gacik.testingFramewokTutorial.P1SimpleAssert.exe1;
+
+public class StandardAssert {
+    public static void main(String[] args) {
+        assert false : "Sth wrong";
+    }
+}
