24ce5163c57f10a2a671d75492cd9a9cb9383ccb
JG <gacik.jakub@gmail.com>
2018-06-06 09:46:45 +0200

    Exe1 solved

--- src/main/java/pl/gacik/testingFramewokTutorial/P1SimpleAssert/exe1/SimpleAssert.java
+++ src/main/java/pl/gacik/testingFramewokTutorial/P1SimpleAssert/exe1/SimpleAssert.java
@@ -3,7 +3,9 @@ package pl.gacik.testingFramewokTutorial.P1SimpleAssert.exe1;
 public class SimpleAssert {
 
     public static void assertion(boolean condition, String failMessage) {
-        // Write you own implementation of keyword assert. As simple as you can.
+        if (!condition) {
+            throw new AssertionError(failMessage);
+        }
     }
 
     public static void main(String[] args) {
